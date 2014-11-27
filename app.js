7ce414922fbcc33b0213e3899a3588ba6067a43c
Andrew Thomas <andrewmthomas87@gmail.com>
2014-11-26 19:09:36 -0700

    Minimum and maximum values for width and height

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ $(document).ready(function() {
 		if (!width || !height) {
 			error('Invalid values');
 		}
+		if (width > 500 || width < 25 || height > 500 || height < 25) {
+			error('Minimum value of 25 and maximum of 500');
+		}
 	});
 });
 
