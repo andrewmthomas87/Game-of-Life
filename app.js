164e2b9fd6e4c4f81a03011c9c990d924e60cda9
Andrew Thomas <andrewmthomas87@gmail.com>
2014-11-26 19:03:34 -0700

    Error badge

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 
+var $error;
+
 $(document).ready(function() {
+	$error = $('div#error');
 	setTimeout(function() {
 		$('form').fadeIn('slow');
 	}, 1000);
@@ -8,7 +11,20 @@ $(document).ready(function() {
 		var width = parseInt($(this).find('input:first-child').val(), 10);
 		var height = parseInt($(this).find('input:nth-child(2)').val(), 10);
 		if (!width || !height) {
-			alert('Invalid values');
+			error('Invalid values');
 		}
 	});
 });
+
+function error(text) {
+	$error.html(text);
+	$error.show();
+	$error.css('top', '0');
+	setTimeout(function() {
+		$error.css('top', '-3em');
+		setTimeout(function() {
+			$error.hide();
+			$error.html('');
+		}, 125);
+	}, 2625);
+}
